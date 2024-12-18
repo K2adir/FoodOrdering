84223a9f922a33a8ae5cb6f7cdb3b86c8449f760
Kadir Kirisci <1120278+K2adir@users.noreply.github.com>
2024-12-18 11:37:22 -0500

    supabase local setup

--- src/app/(user)/profile.tsx
+++ src/app/(user)/profile.tsx
@@ -1,14 +1,29 @@
-import { supabase } from '@/lib/supabase';
-import { View, Text, Button } from 'react-native';
+import { supabase } from "@/lib/supabase";
+import { View, Text, Button } from "react-native";
+import { useState } from "react";
+import { Redirect } from "expo-router";
 
 const ProfileScreen = () => {
+  const [signedOut, setSignedOut] = useState(false);
+
+  if (signedOut) {
+    return <Redirect href="/(auth)/sign-up" />;
+  }
+
   return (
     <View>
       <Text>Profile</Text>
-
       <Button
         title="Sign out"
-        onPress={async () => await supabase.auth.signOut()}
+        onPress={async () => {
+          try {
+            // Sign out the user
+            await supabase.auth.signOut();
+            setSignedOut(true); // Immediately trigger redirect
+          } catch (err) {
+            console.error("Sign out error:", err.message);
+          }
+        }}
       />
     </View>
   );
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -15,9 +15,8 @@ const ExpoSecureStoreAdapter = {
   },
 };
 
-const supabaseUrl = "https://rjatbylyhxszsgnwnssj.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqYXRieWx5aHhzenNnbnduc3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0NjkzNjksImV4cCI6MjA0NjA0NTM2OX0.hxjw7k5akpphb_fEkkV_6xTFbCKoritACJSVWsMY2hM";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
